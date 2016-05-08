11c81a9d8b609b37075a8e1d152e4a56e2931b44
josealejolibreros <jose.libreros@correounivalle.edu.co>
2016-05-08 14:15:39 -0500

    Create tarea.sh

---
+++ tarea.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# calcula el promedio de una serie de números
+
+SCORE="0"
+AVERAGE="0"
+SUM="0"
+NUM="0"
+
+while true; do
+
+	echo -n "Digite su puntaje [0-100%] ('q' para salir): ";
+	read SCORE;
+	
+	if (("$SCORE" < "0" )) || (("$SCORE" > "100" )); then
+		echo "Pórtese serio. Vamos, inténtelo de nuevo: "
+	elif [ "$SCORE" == "q" ]; then
+		echo "Promedio: $AVERAGE%."
+		break
+	else 
+		curl --silent --request POST --header "X-THINGSPEAKAPIKEY: PK6SB6LPTG17KTNZ" --data "field1=$SCORE" "http://api.thingspeak.com/update"
+		SUM=$[$SUM + $SCORE]
+		NUM=$[$NUM + 1]
+		AVERAGE=$[$SUM / $NUM]
+	fi
+done
+
+echo "Saliendo."
